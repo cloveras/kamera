04de2330f410a91c9dcdb176f19fda2dce397ee3
Christian L <cl@superelectric.net>
2015-12-03 19:36:58 +0100

    Now perfect.

--- copy-latest-image.sh
+++ copy-latest-image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Copies the latest *.jpg file found in current directory and subdirectories to latest.jpg
+# Copies the latest *.jpg file found in current directory and subdirectories to ./latest.jpg
 
 # Go the the directory wher the seach for *jpg will be done.
 cd /home/2/s/superelectric/www/viktun/kamera
 
-# Copy the latest *.jpg, latest.jpg in the current directory.
+# Copy the latest *.jpg image to latest.jpg in the current directory.
 find . -type f | grep jpg | grep -v latest | sort -r | head -n1 | xargs -I '{}' cp '{}' latest.jpg

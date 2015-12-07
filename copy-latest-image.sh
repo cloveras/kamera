8f1c9715ef6b66fb4df75279d0be6b495c82ddbd
Christian L <cl@superelectric.net>
2015-12-07 13:10:42 +0100

    Only searching today's directory.

--- copy-latest-image.sh
+++ copy-latest-image.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Copies the latest *.jpg file found in current directory and subdirectories to ./latest.jpg
+# Copies the latest *.jpg file in the "today" directory to latest.jpg in the current directory.
+
+# Find today's year, month and day
+today="$(date +'%Y%m%d')"
 
 # Go the the directory wher the seach for *jpg will be done.
 cd /home/2/s/superelectric/www/viktun/kamera
 
-# Copy the latest *.jpg image to latest.jpg in the current directory.
-find . -type f | grep jpg | grep -v latest | sort -r | head -n1 | xargs -I '{}' cp '{}' latest.jpg
+# If the "today" directory exists, copy the latest *.jpg image to latest.jpg in the current directory.
+[ -d $today ] && find $today -type f | grep jpg | grep -v latest | grep -v small | sort -r | head -n1 | xargs -I '{}' cp '{}' latest.jpg
